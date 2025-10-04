63e354b294950b8ec39c28b1b5f2225c0b6b4360
Stan <75632684+Stan-tab@users.noreply.github.com>
2025-10-04 07:52:00 -0700

    feat:Small changes

--- components/dictioanary.py
+++ components/dictioanary.py
@@ -2,10 +2,12 @@ from aiogram import html
 
 sentences = {
     "en": {
-        "greeting": f"Hello $|.\nWelcome to {html.italic('funny')} audio bot 😎.\nI hope you will love it ❤️.\nIf you want to know how bot works enter /help"
+        "greeting": f"Hello $|.\nWelcome to {html.italic('funny')} audio bot 😎.\nI hope you will love it ❤️.\nIf you want to know how bot works enter /help",
+        "help": f"This bot is made to entertain you 🤪.\nYou can send audio to me and i save it for all users 😍.\n Btw, you can call me in any chat and i will show audio that i have 😉.\n Let's begin 🎉.",
     },
     "ru": {
-        "greeting": f"Привет $|.\nДобро пожаловать в {html.italic('забавный')} аудиобот 😎.\nНадеюсь, вам понравится ❤️\nЕсли вы хотите узнать, как работает бот, введите /help"
+        "greeting": f"Привет $|.\nДобро пожаловать в {html.italic('забавный')} аудиобот 😎.\nНадеюсь, вам понравится ❤️\nЕсли вы хотите узнать, как работает бот, введите /help",
+        "help": f"Этот бот создан, чтобы развлекать вас 🤪.\nВы можете отправлять мне аудио, и я сохраню его для всех пользователей 😍.\nTaкже, вы можете позвонить мне в любой чат, и я покажу вам мои аудио 😉.\nДавайте начнём 🎉.",
     },
 }
 
--- main.py
+++ main.py
@@ -7,13 +7,18 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 import aiogram.filters as filters
-from aiogram.types import Message, audio
+from aiogram.types import Message
 from components.dictioanary import useSentences
 from components.subFunctions import desiredSentence
 
 load_dotenv()
 TOKEN = getenv("API_TOKEN")
 dp = Dispatcher()
+
+
+async def main():
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
 
 
 @dp.message(filters.CommandStart())
@@ -28,13 +33,21 @@ async def welcome(message: Message):
         print(f"Error occured: {e}")
 
 
-# @dp.message(filters.Command("help"))
-# async def help():
+@dp.message(filters.Command("help"))
+async def help(message: Message):
+    answer = desiredSentence(message.from_user.language_code, ["help"])
+    try:
+        await message.answer(answer)
+    except Exception as e:
+        print(f"Error occured: {e}")
 
 
-async def main():
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await dp.start_polling(bot)
+@dp.message()
+async def getAudio(message: Message):
+    if message.voice:
+        await message.answer_voice(voice=message.voice.file_id)
+    elif message.audio:
+        await message.answer_audio(audio=message.audio.file_id)
 
 
 if __name__ == "__main__":
